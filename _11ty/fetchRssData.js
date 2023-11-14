a3c1a3345056d5e786b8245ad0068f3e932a01c3
Tom Casavant <tfcasavant@gmail.com>
2023-11-14 16:10:13 -0500

    Update fetchRssData.js

--- _11ty/fetchRssData.js
+++ _11ty/fetchRssData.js
@@ -1,14 +1,46 @@
-// fetchRssData.js
+// generateRssHtml.js
 const fetch = require('node-fetch');
-const fs = require('fs').promises;
 
-module.exports = async function() {
+async function generateRssHtml() {
   try {
     const url = 'https://tomcasavant.glitch.me/index.xml';
     const response = await fetch(url);
-    const xmlData = await response.text();
-    await fs.writeFile('./content/_data/rss.xml', xmlData); // Save the XML data to a file
+    const rssData = await response.text();
+
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(rssData, 'text/xml');
+
+    let html = '<div class="feed">';
+    html += `<h1>${xmlDoc.querySelector('title').textContent}</h1>`;
+    html += `<p><strong>Updated:</strong> ${xmlDoc.querySelector('updated').textContent}</p>`;
+    html += `<p><strong>Author:</strong> ${xmlDoc.querySelector('author name').textContent}</p>`;
+
+    const entries = xmlDoc.querySelectorAll('entry');
+    entries.forEach((entry) => {
+      html += '<div class="entry">';
+      html += `<h2>${entry.querySelector('title').textContent}</h2>`;
+      html += `<p><strong>Updated:</strong> ${entry.querySelector('updated').textContent}</p>`;
+      html += `<p>${entry.querySelector('summary').textContent}</p>`;
+      
+      html += '<p><strong>Categories:</strong>';
+      const categories = entry.querySelectorAll('category');
+      categories.forEach((category, index) => {
+        html += `${category.getAttribute('term')}`;
+        if (index !== categories.length - 1) {
+          html += ', ';
+        }
+      });
+      html += '</p>';
+      
+      html += '</div>';
+    });
+
+    html += '</div>';
+    return html;
   } catch (error) {
-    console.error('Error fetching or saving RSS feed:', error);
+    console.error('Error fetching or processing RSS data:', error);
+    return '<p>No RSS data available.</p>';
   }
-};
+}
+
+module.exports = generateRssHtml;
