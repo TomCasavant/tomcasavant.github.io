714ea15c21874e1cec99b8298857a5a0007b8e1a
Tom Casavant <tfcasavant@gmail.com>
2024-06-05 22:44:09 -0400

    Added webmention counts

--- _11ty/filters.js
+++ _11ty/filters.js
@@ -2,7 +2,9 @@ const { DateTime } = require('luxon');
 
 module.exports = {
     getWebmentionsForUrl: (webmentions, url) => {
-        return webmentions.children.filter(entry => entry['wm-target'] === url)
+        return webmentions.children
+            .filter(entry => entry['wm-target'] === url)
+            .sort((a, b) => new Date(a.published) - new Date(b.published));
     },
     size: (mentions) => {
         return !mentions ? 0 : mentions.length
@@ -12,5 +14,8 @@ module.exports = {
     },
     readableDateFromISO: (dateStr, formatStr = "dd LLL yyyy 'at' hh:mma") => {
         return DateTime.fromISO(dateStr).toFormat(formatStr);
-    }
+    },
+    readableDateTimeFromISO: (dateStr, formatStr = "dd LLL yyyy 'at' hh:mma") => {
+    	return DateTime.fromISO(dateStr).toFormat(formatStr);
+ 	},
 }
