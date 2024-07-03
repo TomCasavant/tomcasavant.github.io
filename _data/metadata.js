a1e1de468940465f9f8ae264586130daead1667d
Tom Casavant <tfcasavant@gmail.com>
2024-07-03 16:22:59 +0000

    Fix og tags

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Tom Casavant",
 	url: "https://tomcasavant.com/",
 	language: "en",
-	description: "",
+	description: "Blog about things",
 	author: {
 		name: "Tom Casavant",
 		email: "tfcasavant@gmail.com",
