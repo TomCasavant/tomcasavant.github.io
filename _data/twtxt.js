6ee0f0febd9be84a8200e353d4f814b484f02106
Tom Casavant <tfcasavant@gmail.com>
2024-07-08 14:32:49 -0400

    Update twtxt.js

--- _data/twtxt.js
+++ _data/twtxt.js
@@ -1,17 +1,16 @@
 module.exports = function () {
     const nickname = 'tom';
     const twtxtUrl = 'https://tomcasavant.com/twtxt.txt';
-  
+
     const posts = [
-      `2024-07-08T13:50:00Z /nick ${nickname}`,
-      `2024-07-08T13:51:00Z /twturl ${twtxtUrl}`,
-      `2024-07-08T14:18:30-04:00 Hello world, this is your captain speaking`,
+        `2024-07-08T13:50:00Z\t/nick ${nickname}`,
+        `2024-07-08T13:51:00Z\t/twturl ${twtxtUrl}`,
+        `2024-07-08T14:18:30-04:00\tHello world, this is your captain speaking`,
     ];
-  
+
     const twtxtContent = posts.join('\n');
-  
+
     return {
       content: twtxtContent,
     };
-  };
-  
+};
